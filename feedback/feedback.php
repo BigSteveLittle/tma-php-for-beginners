2c8a49b63e39742640f3e696674bcbc3b9571086
BigSteveLittle <bigsteve@redandblackzone.com>
2022-11-07 15:23:22 +1100

    Initial Commit for the 'Feedback' Sub-Project

---
+++ feedback/feedback.php
@@ -0,0 +1,18 @@
+<?php include './includes/header.php' ?>
+            <h2>Feedback</h2>
+                <div class="card my-3">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis! </p>
+                    </div>
+                </div>
+                <div class="card my-3">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis! </p>
+                    </div>
+                </div>
+                <div class="card my-3">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis! </p>
+                    </div>
+                </div>
+<?php include './includes/footer.php' ?>
---
+++ feedback/includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE php>
+<php lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <title>Leave Feedback</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="./index.php">RedAndBlackZone</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./feedback.php">Feedback</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <div class="container d-flex flex-column align-items-center">
+            <img src="./images/logo.png" class="w-25 mb-3" alt="">
